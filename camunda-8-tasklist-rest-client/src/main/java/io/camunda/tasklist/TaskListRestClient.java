b9970dc69e7026419725c73bfef78cffa53fe415
Dave Paroulek <david.paroulek@camunda.com>
2023-09-24 22:30:37 -0400

    java rest client - implemented getForm, completeTask, getTask

--- camunda-8-tasklist-rest-client/src/main/java/io/camunda/tasklist/TaskListRestClient.java
+++ camunda-8-tasklist-rest-client/src/main/java/io/camunda/tasklist/TaskListRestClient.java
@@ -18,6 +18,7 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class TaskListRestClient {
 
@@ -58,46 +59,46 @@ public class TaskListRestClient {
     this.authentication.authenticate(this);
   }
 
-  public HttpResponse<String> post(String endPoint, String body, Class responseClass)
+  public HttpResponse<String> post(String endPoint, String body)
       throws TaskListException, TaskListRestException {
-    return postOrPatch("POST", endPoint, body, responseClass);
+    return postOrPatch("POST", endPoint, body);
   }
 
-  public HttpResponse<String> patch(String endPoint, String body, Class responseClass)
+  public HttpResponse<String> patch(String endPoint, String body)
       throws TaskListException, TaskListRestException {
-    return postOrPatch("PATCH", endPoint, body, responseClass);
+    return postOrPatch("PATCH", endPoint, body);
   }
 
-  private HttpResponse<String> postOrPatch(String method, String endPoint, String body, Class responseClass)
-  throws TaskListException, TaskListRestException {
+  private HttpResponse<String> postOrPatch(String method, String endPoint, String body)
+      throws TaskListException, TaskListRestException {
 
-      if(accessTokenResponse == null) {
-        this.authentication.authenticate(this);
+    if(accessTokenResponse == null) {
+      this.authentication.authenticate(this);
+    }
+
+    HttpResponse<String> response = doPostOrPatch(method, endPoint, body);
+
+    //If we get a 401 that might mean the access token has expired. Try to refresh the token
+    if(response.statusCode() == 401) {
+      this.authentication.authenticate(this);
+      response = doPostOrPatch(method, endPoint, body);
+    }
+
+    if(response.statusCode() == 200) {
+      return response;
+    } else if (response.statusCode() == 400) {
+
+      JsonUtils<ErrorResponse> jsonUtils = new JsonUtils<>(ErrorResponse.class);
+      try {
+        ErrorResponse errorResponse = jsonUtils.fromJson(response.body());
+        throw new TaskListRestException(errorResponse);
+      } catch (JsonProcessingException e) {
+        throw new TaskListException("Unable to parse error response", e);
       }
 
-      HttpResponse<String> response = doPostOrPatch(method, endPoint, body);
-
-      //If we get a 401 that might mean the access token has expired. Try to refresh the token
-      if(response.statusCode() == 401) {
-        this.authentication.authenticate(this);
-        response = doPostOrPatch(method, endPoint, body);
-      }
-
-      if(response.statusCode() == 200) {
-        return response;
-      } else if (response.statusCode() == 400) {
-
-        JsonUtils<ErrorResponse> jsonUtils = new JsonUtils<>(ErrorResponse.class);
-        try {
-          ErrorResponse errorResponse = jsonUtils.fromJson(response.body());
-          throw new TaskListRestException(errorResponse);
-        } catch (JsonProcessingException e) {
-          throw new TaskListException("Unable to parse error response", e);
-        }
-
-      } else {
-        throw new TaskListException("Unexpected response from post", new RuntimeException(response.statusCode() + " " + response.body()));
-      }
+    } else {
+      throw new TaskListException("Unexpected response from post", new RuntimeException(response.statusCode() + " " + response.body()));
+    }
   }
 
   private HttpResponse<String> doPostOrPatch(String method, String endPoint, String body) throws TaskListException {
@@ -121,13 +122,66 @@ public class TaskListRestClient {
 
   }
 
+  private HttpResponse<String> get(String endPoint)
+      throws TaskListException, TaskListRestException {
+
+    if(accessTokenResponse == null) {
+      this.authentication.authenticate(this);
+    }
+
+    HttpResponse<String> response = doGet(endPoint);
+
+    //If we get a 401 that might mean the access token has expired. Try to refresh the token
+    if(response.statusCode() == 401) {
+      this.authentication.authenticate(this);
+      response = doGet(endPoint);
+    }
+
+    if(response.statusCode() == 200) {
+      return response;
+    } else if (response.statusCode() == 400) {
+
+      JsonUtils<ErrorResponse> jsonUtils = new JsonUtils<>(ErrorResponse.class);
+      try {
+        ErrorResponse errorResponse = jsonUtils.fromJson(response.body());
+        throw new TaskListRestException(errorResponse);
+      } catch (JsonProcessingException e) {
+        throw new TaskListException("Unable to parse error response", e);
+      }
+
+    } else {
+      throw new TaskListException("Unexpected response from post", new RuntimeException(response.statusCode() + " " + response.body()));
+    }
+  }
+
+  private HttpResponse<String> doGet(String endPoint) throws TaskListException {
+    try {
+
+      HttpRequest request = HttpRequest.newBuilder()
+          .uri(new URI(endPoint))
+          .header("content-type", "application/json")
+          .header("Authorization", "Bearer " + accessTokenResponse.getAccess_token())
+          .timeout(Duration.ofSeconds(10))
+          .GET()
+          .build();
+
+      return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+    } catch (URISyntaxException e) {
+      throw new TaskListException("Endpoint URL must be a valid URI", e);
+    } catch (IOException | InterruptedException e) {
+      throw new TaskListException("Unable to complete request", e);
+    }
+
+  }
+
   public List<TaskSearchResponse> searchTasks(TaskSearchRequest request) throws TaskListException, TaskListRestException {
     JsonUtils<TaskSearchRequest> jsonRequest = new JsonUtils<>(TaskSearchRequest.class);
     try {
 
       String body = jsonRequest.toJson(request);
       String endpoint = taskListBaseUrl +  "/v1/tasks/search";
-      HttpResponse<String> response = post(endpoint, body, TaskSearchResponse.class);
+      HttpResponse<String> response = post(endpoint, body);
 
       // TODO: update JsonUtils if this works
       JsonUtils<List> jsonResponse = new JsonUtils<>(List.class);
@@ -142,13 +196,45 @@ public class TaskListRestClient {
     }
   }
 
+  public TaskResponse getTask(String taskId) throws TaskListException, TaskListRestException {
+
+    try {
+
+      String endpoint = taskListBaseUrl +  "/v1/tasks/" + taskId;
+      HttpResponse<String> response = get(endpoint);
+
+      JsonUtils<TaskResponse> jsonResponse = new JsonUtils<>(TaskResponse.class);
+      return jsonResponse.fromJson(response.body());
+
+    } catch (JsonProcessingException e) {
+      throw new TaskListException("Unable to parse FormResponse to json", e);
+    }
+
+  }
+
+
+  public FormResponse getForm(String processDefinitionKey, String formId) throws TaskListException, TaskListRestException {
+
+    try {
+      String endpoint = taskListBaseUrl +  "/v1/forms/" + formId + "?processDefinitionKey=" + processDefinitionKey;
+      HttpResponse<String> response = get(endpoint);
+
+      // TODO: update JsonUtils if this works
+      JsonUtils<FormResponse> jsonResponse = new JsonUtils<>(FormResponse.class);
+      return jsonResponse.fromJson(response.body());
+
+    } catch (JsonProcessingException e) {
+      throw new TaskListException("Unable to parse FormResponse to json", e);
+    }
+  }
+
   public TaskResponse assignTask(String taskId, TaskAssignRequest request) throws TaskListException, TaskListRestException {
     JsonUtils<TaskAssignRequest> jsonRequest = new JsonUtils<>(TaskAssignRequest.class);
     try {
 
       String body = jsonRequest.toJson(request);
       String endpoint = taskListBaseUrl + "/v1/tasks/"+taskId+"/assign";
-      HttpResponse<String> response = patch(endpoint, body, TaskSearchResponse.class);
+      HttpResponse<String> response = patch(endpoint, body);
       JsonUtils<TaskResponse> jsonResponse = new JsonUtils<>(TaskResponse.class);
       return jsonResponse.fromJson(response.body());
 
@@ -160,7 +246,31 @@ public class TaskListRestClient {
   public TaskResponse unassignTask(String taskId) throws TaskListException, TaskListRestException {
     try {
       String endpoint = taskListBaseUrl + "/v1/tasks/"+taskId+"/unassign";
-      HttpResponse<String> response = patch(endpoint, "", TaskSearchResponse.class);
+      HttpResponse<String> response = patch(endpoint, "");
+      JsonUtils<TaskResponse> jsonResponse = new JsonUtils<>(TaskResponse.class);
+      return jsonResponse.fromJson(response.body());
+
+    } catch (JsonProcessingException e) {
+      throw new TaskListException("Unable to parse TaskSearchRequest to json", e);
+    }
+  }
+
+  public TaskResponse completeTask(String taskId, Map<String, Object> variablesMap) throws TaskListException, TaskListRestException {
+
+    //TODO: convert variablesMap to TaskCompleteRequest
+    TaskCompleteRequest taskCompleteRequest = new TaskCompleteRequest();
+    VariableInput variableInput = new VariableInput();
+    variableInput.setName("message");
+    variableInput.setValue("Note that the tasklist rest api client you are using is not fully implemented yet!");
+    List<VariableInput> variableInputs = new ArrayList<>();
+    variableInputs.add(variableInput);
+    taskCompleteRequest.setVariables(variableInputs);
+
+    JsonUtils<TaskCompleteRequest> jsonRequest = new JsonUtils<>(TaskCompleteRequest.class);
+    try {
+      String body = jsonRequest.toJson(taskCompleteRequest);
+      String endpoint = taskListBaseUrl + "/v1/tasks/"+taskId+"/complete";
+      HttpResponse<String> response = patch(endpoint, body);
       JsonUtils<TaskResponse> jsonResponse = new JsonUtils<>(TaskResponse.class);
       return jsonResponse.fromJson(response.body());
 
