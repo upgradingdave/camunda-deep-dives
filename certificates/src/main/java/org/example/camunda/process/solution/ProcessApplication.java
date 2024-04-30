7f24a46e06b79643726e14e9bb46dbd0cde81369
Dave Paroulek <david.paroulek@camunda.com>
2024-04-30 16:33:12 -0400

    latest zeebe client spring app

--- certificates/src/main/java/org/example/camunda/process/solution/ProcessApplication.java
+++ certificates/src/main/java/org/example/camunda/process/solution/ProcessApplication.java
@@ -1,15 +1,12 @@
 package org.example.camunda.process.solution;
 
-import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Deployment(resources = "classpath*:/models/*.*")
 public class ProcessApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(ProcessApplication.class, args);
   }
